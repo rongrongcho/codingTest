f8df679e3787e72da838a15477c053c41ad55b02
Chorong Lee <119823081+rongrongcho@users.noreply.github.com>
2023-09-15 16:37:57 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 212 ms, Memory: 20976 KB -BaekjoonHub

---
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); 
+        for(int i=0; i<T; i++){
+            br.readLine(); 
+            int n = Integer.parseInt(br.readLine());
+            BigInteger sum = new BigInteger("0"); 
+            for(int j=0; j<n; j++){
+                sum = sum.add(BigInteger.valueOf(Long.parseLong(br.readLine())));
+            }
+            System.out.println((String.valueOf(sum.remainder(BigInteger.valueOf(n))).equals("0"))? "YES":"NO");
+        }
+    }
+}
