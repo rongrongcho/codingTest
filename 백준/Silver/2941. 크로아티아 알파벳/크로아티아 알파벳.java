b530fa3b19b457e352203ff34fb061d3ca47905c
Chorong Lee <119823081+rongrongcho@users.noreply.github.com>
2023-12-20 15:19:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();        
+        String[] croatiaAlphabet = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+		int x = 0;
+		for (int i = 0; i < croatiaAlphabet.length; i++) {		
+            x = s.indexOf(croatiaAlphabet[i]);            
+			if (x >= 0) { 
+				s = s.replaceAll(croatiaAlphabet[i], "A");
+			}
+		}
+		System.out.println(s.length());
+	}
+}
