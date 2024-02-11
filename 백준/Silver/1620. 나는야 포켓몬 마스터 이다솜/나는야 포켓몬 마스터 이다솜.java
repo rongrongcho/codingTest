8c7bab3eeb1d7f2d5dcf923c177d95bf3346efd8
Chorong Lee <119823081+rongrongcho@users.noreply.github.com>
2024-02-11 23:58:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 520 ms, Memory: 52684 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String,String> hashMap = new HashMap<>();
+        
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            String num = Integer.toString(i+1);
+            hashMap.put(s,num);
+            hashMap.put(num,s);
+        }
+        for (int i = 0; i < M; i++) {
+               sb.append(hashMap.get(br.readLine())).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
