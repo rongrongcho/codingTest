339fbec828d37173598cc02e3f478acb1f89dcc3
Chorong Lee <119823081+rongrongcho@users.noreply.github.com>
2024-04-29 14:18:16 +0900

    [Silver IV] Title: 문자열, Time: 132 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        int result = A.length();
+
+        for (int i = 0; i < B.length() - A.length() + 1; i++) {
+            int temp = 0;
+            for (int j = 0; j < A.length(); j++) {
+                if (A.charAt(j) != B.charAt(j + i)) {
+                    temp++;
+                }
+            }
+            if (result > temp) {
+                result = temp;
+            }
+        }
+        System.out.println(result);
+    }
+}
